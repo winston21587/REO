ab036db16d7496161df2fdaab3ae523fe36f6b0b
Winston <tabotabowinston@gmail.com>
2025-11-28 23:49:22 +0800

    no status

--- app/Models/researcher_files.php
+++ app/Models/researcher_files.php
@@ -12,7 +12,7 @@ class Researcher_files extends Model
     protected $table = 'researcher_files';
 
     protected $fillable = [
-        'research_title_id', // This matches the foreign key column
+        //'research_title_id', // This matches the foreign key column
         'filename',
         'file_path',
         'file_type',   

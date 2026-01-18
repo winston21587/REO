2b6d5a9c8998a7afc42c26e2a3d87d7eb674dc30
Winston <tabotabowinston@gmail.com>
2026-01-18 18:01:18 +0800

    a

--- resources/views/admin/applications.blade.php
+++ resources/views/admin/applications.blade.php
@@ -170,7 +170,7 @@
                                         </div>
                                     </div>
                                 </div>
-                            </td>test
+                            </td>
                         </tr>
                         @empty
                         <tr>

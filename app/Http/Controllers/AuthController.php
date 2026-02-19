bed9bf57fa22f58dc4f82d20a18ff701c6e4e6f6
Winston <tabotabowinston@gmail.com>
2026-02-19 23:27:06 +0800

    logging out redirect to the login page instead of homepage

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -181,7 +181,7 @@ class AuthController extends Controller
         $request->session()->invalidate();
         $request->session()->regenerateToken();
 
-        return redirect()->route('index');
+        return redirect()->route('login');
     }   
 
     public function acceptTerms(Request $request)

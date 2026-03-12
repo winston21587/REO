e7397c5bf706ce57c6bb1b74167c51ad32658aa3
janusinss <janusdominic0@gmail.com>
2026-03-12 22:59:52 +0800

    no vite

--- resources/views/components/user_layout.blade.php
+++ resources/views/components/user_layout.blade.php
@@ -18,8 +18,6 @@
     <!-- Alpine.js Core -->
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 
-    <!-- @vite(['resources/css/app.css', 'resources/js/app.js']) -->
-
     <style>
         body {
             font-family: 'Inter', sans-serif;

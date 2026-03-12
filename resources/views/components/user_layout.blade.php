84c459fd1070c1fb9a4515272d017cd948e099ea
janusinss <janusdominic0@gmail.com>
2026-03-12 22:58:38 +0800

    vite

--- resources/views/components/user_layout.blade.php
+++ resources/views/components/user_layout.blade.php
@@ -18,7 +18,7 @@
     <!-- Alpine.js Core -->
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <!-- @vite(['resources/css/app.css', 'resources/js/app.js']) -->
 
     <style>
         body {

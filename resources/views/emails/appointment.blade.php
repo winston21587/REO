ffe832c2904a5b44fad37987f4e87b65c4d55d0f
Winston <tabotabowinston@gmail.com>
2026-04-21 00:21:27 +0800

    meetings and agenda time format is removed exept the commitee appointments

--- resources/views/emails/appointment.blade.php
+++ resources/views/emails/appointment.blade.php
@@ -2,7 +2,7 @@
 
 <p>Your research titled <strong>{{ $research->Study_Protocol_title }}</strong> has been marked as <strong>For Initial Review</strong>.</p>
 
-<p><strong>Appointment Date:</strong> {{ \Carbon\Carbon::parse($appointment->appointment_date)->format('F j, Y, g:i A') }}</p>
+<p><strong>Appointment Date:</strong> {{ \Carbon\Carbon::parse($appointment->appointment_date)->format('F j, Y') }}</p>
 
 <p>Please be ready on that date for your initial review session.</p>
 
